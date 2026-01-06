154a11e00b4dd6277d268447835f36829237b251
SaifullahCodes <saifullah.naseer.dev@gmail.com>
2026-01-06 22:29:07 +0500

    Add audio_api.py file

--- audio_api.py
+++ audio_api.py
@@ -111,4 +111,5 @@ async def predict_endpoint(file: UploadFile = File(...)):
 if __name__ == "__main__":
     # Get port from environment variable (Required for Cloud)
     port = int(os.environ.get("PORT", 8000))
+
     uvicorn.run(app, host="0.0.0.0", port=port)
